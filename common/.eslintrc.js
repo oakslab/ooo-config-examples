41175b3de7d81af11de11e877c69e306e2d7575c
Filip Novotný <filip@novotny.je>
2021-01-05 17:33:26 +0100

    move React-specific config to frontend folder

--- common/.eslintrc.js
+++ common/.eslintrc.js
@@ -1,29 +1,16 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 2020,
     sourceType: 'module',
   },
   extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  ignorePatterns: ['node_modules/*', '/codegen/*', '/public/*', '/tests/**'],
+  ignorePatterns: ['node_modules/*', '/tests/**'],
   rules: {
-    'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }], // Allow quote marks in text
-    'react/prop-types': 'off', // Allow not providing PropTypes as we use FC<Props> to type props
-    'react/react-in-jsx-scope': 'off', // Allow not importing React as Next imports it automatically
     '@typescript-eslint/ban-ts-comment': 'off', // Allow @ts-ignore
     '@typescript-eslint/explicit-module-boundary-types': 'off', // Allow implicit function return types
   },
